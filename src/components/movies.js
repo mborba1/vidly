0673b044e5a99796e0908b8d153b51969da65d82
Margareth Borba <mborba1@icloud.com>
2021-11-13 16:25:15 -0500

    added allgenre functionality

--- src/components/movies.js
+++ src/components/movies.js
@@ -15,7 +15,8 @@ class Movies extends Component {
     };
 
     componentDidMount(){
-        this.setState({ movies: getMovies(), genres: getGenres() });
+        const genres = [{ name: 'All Genres' }, ...getGenres()]
+        this.setState({ movies: getMovies(), genres });
     }
 
     handleDelete = movie => {
@@ -41,7 +42,7 @@ class Movies extends Component {
     };
     handleGenreSelect = genre => {
         //console.log(genre)
-        this.setState({ selectedGenre: genre });
+        this.setState({ selectedGenre: genre, currentPage: 1 });
     }
 
     render(){
@@ -50,7 +51,7 @@ class Movies extends Component {
 
         if (count === 0) return <p>There are no movies in the databse.</p>
         
-        const filtered = selectedGenre ? allMovies.filter(m => m.genre._id === selectedGenre._id) : allMovies;
+        const filtered = selectedGenre && selectedGenre._id ? allMovies.filter(m => m.genre._id === selectedGenre._id) : allMovies;
         const movies = paginate(filtered, currentPage, pageSize)
         return (
             <div className="row">
